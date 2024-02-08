4cd6254e611b5d825ad1fc8a72512d56c3bccd87
Matt <mattb@nsccreative.com>
2024-02-08 11:55:34 +0000

    Removed the Slack function from the Bacula script

--- Bacula/export.sh
+++ Bacula/export.sh
@@ -5,22 +5,14 @@
 
 LOG=/Pool01/Bacula/Exports/logs/$(date +\%Y-\%m-\%d).log
 
-SLACK() {
-    SLACKCHANNEL="anton"
-    SLACKUSERNAME="Anton"
-    SLACKEMOJI=":nas:"
-    SLACKWEBHOOK=https://hooks.slack.com/services/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
-    SLACKPAYLOAD="payload={\"channel\": \"anton\", \"username\": \"$SLACKUSERNAME\", \"text\": \"$SLACKMESSAGE\", \"icon_emoji\": \"$SLACKEMOJI\"}"
-    curl -X POST --data-urlencode "$SLACKPAYLOAD" "$SLACKWEBHOOK"
-}
-
 MKDIR(){
     mkdir /Pool01/Bacula/Exports/tar/$(date +\%Y-\%m-\%d)
 }
 
 COPY(){
     cp -r /opt/bacula/etc/ /Pool01/Bacula/Exports/tar/$(date +\%Y-\%m-\%d)
-    cp /Pool01/Bacula/Exports/dailyBackup/$(date +\%Y-\%m-\%d --date="1 days ago") /Pool01/Bacula/Exports/tar/$(date +\%Y-\%m-\%d)
+    cp -r /Pool01/Bacula/BootStrap/* /Pool01/Bacula/Exports/tar/$(date +\%Y-\%m-\%d)
+    cp -r /Pool01/Bacula/Exports/dailyBackup/$(date +\%Y-\%m-\%d --date="1 days ago") /Pool01/Bacula/Exports/tar/$(date +\%Y-\%m-\%d)
 }
 
 TAR(){
@@ -30,6 +22,8 @@ TAR(){
     0) echo "TAR succsessful" | tee -a $LOG;;
     *) echo "TAR failed" | tee -a $LOG;;
     esac
+    cp /Pool01/Bacula/Exports/tar/$(date +\%Y-\%m-\%d).tar.gz /Pool01/WeeklyBackups/
+    chown matt:'it group' /Pool01/WeeklyBackups/$(date +\%Y-\%m-\%d).tar.gz
 }
 
 EXPORT() {
@@ -44,8 +38,10 @@ EXPORT() {
 
 MAIN() {
     echo $(date +\%Y-\%m-\%d) | tee -a $LOG
+    MKDIR
+    COPY
     TAR
-    EXPORT
+    #EXPORT
     echo "Export Complete for $(date +\%Y-\%m-\%d)" | tee -a $LOG
 }
 
